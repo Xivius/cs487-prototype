097dc565114bfa8a2a90a11c3a45bb9f122fc942
Xivius <nsong4@hawk.iit.edu>
2020-05-01 18:26:07 -0500

    Add button link

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,6 +1,9 @@
-function () {
+document.getElementById('drop-off-back-btn').addEventListener(
+  'click', function(event) {
+    console.log("Going back.");
+    event.preventDefault();
+    window.history.go(-1);
+});
 
-}
-
-var backButton = document.getElementById("");
-table.eventListener();
+/*var backButton = document.getElementById("");
+table.eventListener(); */
