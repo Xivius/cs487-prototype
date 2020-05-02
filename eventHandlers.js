9870f7a7261aaef99fbb04ef029fec3304ad749d
Mayank Phadke <mphadke1@hawk.iit.edu>
2020-05-01 19:10:54 -0500

    completed login functionality

--- eventHandlers.js
+++ eventHandlers.js
@@ -5,4 +5,24 @@ function main() {
         history.back();
     })
 
+    document.getElementById("login").addEventListener("click", (e) => {
+        e.preventDefault();
+        var email = document.getElementById("email").value.trim();
+        var password = document.getElementById("password").value.trim();
+        var error = document.getElementById("error");
+
+        if (validateEmail(email) && password !== "") {
+            error.classList.add("d-none");
+            if(email.includes("admin")) {
+                window.location.href = "management.html"
+            }
+        } else {
+            error.classList.remove("d-none");
+        }
+        
+    })
+
+    function validateEmail(email) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+    }
 }
